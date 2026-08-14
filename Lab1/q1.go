2a7604b5da1d8674f8bb16161bf87607a2018abf
Manju Karwasra <karwasramanju4@gmail.com>
2026-08-14 19:39:10 +0530

    Add Lab1 Go programs

---
+++ Lab1/q1.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("First code. Manju")
+
+	var a int = 2
+	var b int = 3
+
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+
+	//float
+	fmt.Println("Float")
+
+	var x float64 = 2
+	var y float64 = 3
+
+	fmt.Println(x + y)
+	fmt.Println(x - y)
+	fmt.Println(x * y)
+	fmt.Println(x / y)
+}
